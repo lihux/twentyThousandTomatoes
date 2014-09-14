8d810af7689798b0c1958f72bcb0b1ae244f7cc0
李辉 <hui.li@renren-inc.com>
2014-09-14 16:58:55 +0800

    添加i vc，点击编辑个人资料

---
+++ TwentyThousandThomatoes/TwentyThousandThomatoes/Dreams/TTIViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTIViewController.h
+//  TwentyThousandThomatoes
+//
+//  Created by lihux on 14-9-14.
+//  Copyright (c) 2014年 lihux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTIViewController : UIViewController
+
+@end
