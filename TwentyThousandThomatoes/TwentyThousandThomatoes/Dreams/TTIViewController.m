e9f58966e79647cad26eee08aecfb7727fc6b0aa
李辉 <hui.li@renren-inc.com>
2014-09-17 14:17:38 +0800

    i 编辑页完善交互

--- TwentyThousandThomatoes/TwentyThousandThomatoes/Dreams/TTIViewController.m
+++ TwentyThousandThomatoes/TwentyThousandThomatoes/Dreams/TTIViewController.m
@@ -8,7 +8,14 @@
 
 #import "TTIViewController.h"
 
+#import "TTDataSourceManager.h"
+#import "TTI.h"
+
 @interface TTIViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *userHeadButton;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *genderButtons;
+@property (nonatomic, strong) TTI *me;
 
 @end
 
@@ -16,12 +23,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self loadDataFromCoreData];
 }
+
+- (void)loadDataFromCoreData
+{
+    NSArray *iInfos = [[TTDataSourceManager sharedInstance] searchManagedObjectWithEntityName:NSStringFromClass([TTI class])];
+    if (iInfos.count > 0) {
+        self.me = iInfos[0];
+    }
+}
+
+- (void)updateUIForDataChange
+{
+    if (self.me) {
+        TTI *i = self.me;
+        if (i.headImage) {
+        }
+    }
+}
+
+- (IBAction)textFieldEditingDidEnd:(id)sender
+{
+}
+
+- (IBAction)didTapOnGenderButton:(id)sender
+{
+    UIButton *selectedButton = (UIButton *)sender;
+    [self.genderButtons enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        UIButton *genderButton = (UIButton *)obj;
+        genderButton.selected = selectedButton.tag == genderButton.tag ? YES : NO;
+    }];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 /*
